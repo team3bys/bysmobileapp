976d1b4e16e0e6a17aab97b496298409d0cc4b09
Jack Stratton <jack@phroa.net>
2015-06-01 09:09:40 -0700

    Add config.xml, make sure Grunt includes it

--- Gruntfile.js
+++ Gruntfile.js
@@ -146,7 +146,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= yeoman.app %>',
-                    src: '*.html',
+                    src: '*.{html,xml}',
                     dest: '<%= yeoman.dist %>'
                 }]
             }
