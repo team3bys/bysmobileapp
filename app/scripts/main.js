f66d0611caf6a6aac015e04e5a132dce060beddb
Jack Stratton <jack@phroa.net>
2015-06-04 09:55:35 -0700

    Add filtered list widget in place of search bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,4 +12,34 @@ require(["jquery",
 	"jqueryMobile",
 ], function ($) {
   console.log('jQuery version ' + $().jquery + ' installed');
+
+  $(document).on("pageinit", "#index", function() {
+    $("#search").on("listviewbeforefilter", function (e, data) {
+      var $ul = $(this),
+      $input = $(data.input),
+      value = $input.val(),
+      html = "";
+      $ul.html(html);
+      if (value && value.length > 2) {
+        $ul.html("<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>");
+        $ul.listview("refresh");
+        $.ajax({
+          url: "http://gd.geobytes.com/AutoCompleteCity",
+          dataType: "jsonp",
+          crossDomain: true,
+          data: {
+            q: $input.val()
+          }
+        })
+        .then(function (response) {
+          $.each(response, function (i, val) {
+            html += "<li>" + val + "</li>";
+          });
+          $ul.html(html);
+          $ul.listview("refresh");
+          $ul.trigger("updatelayout");
+        });
+      }
+    });
+  });
 });
