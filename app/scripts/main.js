63ff76a6d909ff28688582ee2c5530ee221b1d3f
bysmobile <kirkg@kkheconsulting.com>
2015-12-03 10:20:54 -0800

    update from summer work...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,3 +13,37 @@ require(["jquery",
 ], function ($) {
   console.log('jQuery version ' + $().jquery + ' installed');
 });
+
+
+function getDocHeight(doc) {
+    doc = doc || document;
+    // from http://stackoverflow.com/questions/1145850/get-height-of-entire-document-with-javascript
+    var body = doc.body, html = doc.documentElement;
+    var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+    return height;
+}
+
+function setIframeHeight(id) {
+    var ifrm = document.getElementById(id);
+    var doc = ifrm.contentDocument? ifrm.contentDocument: ifrm.contentWindow.document;
+    ifrm.style.visibility = 'hidden';
+    ifrm.style.height = "10px"; // reset to minimal height in case going from longer to shorter doc
+    ifrm.style.height = getDocHeight( doc ) + 10 + "px";
+    ifrm.style.visibility = 'visible';
+}
+
+// Parse URL Queries
+function url_query( query ) {
+    query = query.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var expr = "[\\?&]"+query+"=([^&#]*)";
+    var regex = new RegExp( expr );
+    var results = regex.exec( window.location.href );
+    if ( results !== null ) {
+        return results[1];
+    } else {
+        return false;
+    }
+}
+                            
+                            
+
